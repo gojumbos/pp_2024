6df5f533fca3fc629451a91dcc42f3a953ff5445
gojumbos <declan.borcich@tufts.edu>
2024-08-28 01:36:30 -0400

    scaling, new link

--- app.py
+++ app.py
@@ -9,11 +9,16 @@ logger = logging.getLogger('logger')
 
 @app.route('/', methods=['GET'])
 def landing_page():
-    # app_controller.curr_env = os.getenv('CURR_ENV')  # dev/prod
-    # app_controller.CURR_ENV_LINK_LIT = constants.HOME_URL_LIT_SEL[app_controller.curr_env]
-    # fav_link = app_controller.CURR_ENV_LINK_LIT.replace("api", "") + "static/favicon.ico"
-    # home_link = app_controller.CURR_ENV_LINK_LIT + "/home"
 
     return render_template('index.html',
                            )
+
+
+# @app.route('/paper', methods=['GET'])
+# def landing_page():
+#
+#     return render_template('index.html',
+#                            )
+
+
 
