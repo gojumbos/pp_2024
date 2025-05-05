6956149ae08146f87a9dd5ba432d241b192588a7
gojumbos <declan.borcich@tufts.edu>
2025-05-04 19:06:09 -0700

    plug in api

---
+++ ai.py
@@ -0,0 +1,44 @@
+
+
+
+
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+starter = "You are a 20 Questions game host. Wait for the user's yes/no questions and respond accordingly. Only say 'Ask your first question' once at the start."
+
+
+model = "gpt-4.1"
+
+# Load environment variables from the .env file
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+print(api_key)
+
+class ai_object:
+
+  def __init__(self):
+    self.client = OpenAI(api_key=api_key)
+    self.conversation  = [
+      {"role": "system", "content": "You are a game chat bot. Keep responses very simple."},
+      {"role": "user", "content": starter}, 
+    ]
+
+  def ai_start_chat(self):
+    response = self.client.responses.create(
+    model=model,
+    input=self.conversation
+    )
+    self.conversation.append({"role": "assistant", "content": response.output_text})
+    return response.output_text
+
+  def ai_chat(self, prompt):
+    self.conversation.append({"role": "user", "content": prompt})
+    response = self.client.responses.create(
+    model=model,
+    input=self.conversation
+    )
+    self.conversation.append({"role": "assistant", "content": response.output_text})
+    return response.output_text
--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, request, jsonify, render_template
 
 import logging
 
+import ai
+
 app = Flask(__name__, static_url_path='/static')
 
 logger = logging.getLogger('logger')
+
+ai_object = ai.ai_object()
 
 
 @app.route('/', methods=['GET'])
@@ -21,13 +25,27 @@ def twenty_q():
                            )
 
 
+@app.route('/start_chat', methods=['POST'])
+def start_chat():
+    # user_message = request.json.get('message', '')
+    # reply = f"You said: {user_message}"
+    reply = ai_object.ai_start_chat()
+    for x in ai_object.conversation:
+        print(x)
+        print("\n")
+    return jsonify({'reply': reply})
+
+
+
+
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.json.get('message', '')
-    
-    #trest
-    reply = f"You said: {user_message}"
-
+    # reply = f"You said: {user_message}"
+    reply = ai_object.ai_chat(user_message)
+    for x in ai_object.conversation:
+        print(x)
+        print("\n")
     return jsonify({'reply': reply})
 
 
