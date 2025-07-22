48f4eba17c869c23da71a0d128d947e7dc8f1a0b
gojumbos <declan.borcich@tufts.edu>
2025-07-22 14:12:32 -0400

    fixing ai convo issues, gitignore

--- ai.py
+++ ai.py
@@ -1,12 +1,18 @@
-
-
-
+import copy
+import re
 
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+from random import randint
 
-starter = "You are a 20 Questions game host. Wait for the user's yes/no questions and respond accordingly. Only say 'Ask your first question' once at the start."
+SYSTEM = ("You are a 20 Questions game host. Wait for the user's yes/no questions and respond accordingly. "
+           "Only say 'Ask your first question' once at the start. Tell me the answer you have in mind as your "
+          "first response inside curly brackets like: {hedgehog}, but don't say 'hedgehog', say the object "
+          "you are thinking of. Do not tell me the answer unless I say something "
+          "like 'I give up' or 'just tell me'. If the question limit of 20 is reached, reveal the answer. I will "
+          "feed you the current question count as well. If the user asks any questions about your 'owner' or "
+          "someone named Declan, respond simply with 'I'm gonna tell Declan you were asking about him :o'")
 
 
 model = "gpt-4.1"
@@ -15,36 +21,95 @@ model = "gpt-4.1"
 load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
-print(api_key)
+# print(api_key)
 
-class ai_object:
-
+class ChatManager:
+  """
+    A very information-insecure implementation chat isolation
+    Avoiding using actual cookie implementations for user anonymity
+  """
   def __init__(self):
-    self.client = OpenAI(api_key=api_key)
-    self.conversation  = [
+    self.all_ais_dict = {}
+    self.last_call_time = None
+    # an ai is a chat
+
+  def add_chat(self):
+    global_id = randint(0,1000)
+    while global_id in self.all_ais_dict:
+      global_id = randint(0,1000)
+    ai = AIObject(global_id=global_id)
+    # ai.ai_start_chat()
+    self.all_ais_dict[ai.my_id] = ai
+    return ai.my_id
+
+  def start_chat(self, global_id):
+    # start chat after adding ai
+    ai = self.all_ais_dict[int(global_id)]
+    return ai.ai_start_chat()
+
+  def get_convo(self, global_id):
+    # get ai given its global id
+
+    return self.all_ais_dict[int(global_id)].conversation
+
+  def send_chat(self, global_id, user_message):
+    return self.all_ais_dict[int(global_id)].ai_chat(user_message)
+
+  def remove_chat(self, ai):
+    del self.all_ais_dict[ai.my_id]
+
+  def clear_all_chats(self):
+    # reset all after time period elapsed
+    self.all_ais_dict = {}
+
+  def chat_count(self):
+    return len(self.all_ais_dict)
+
+
+
+CONVO_STUB = [
       {"role": "system", "content": "You are a game chat bot. Keep responses very simple."},
-      {"role": "user", "content": starter}, 
+      {"role": "user", "content": SYSTEM},
     ]
+
+class AIObject:
+
+  def __init__(self, global_id):
+    self.client = OpenAI(api_key=api_key)
+    self.conversation = copy.deepcopy(CONVO_STUB)
+    self.question_ctr = 1
+    self.my_id = global_id
+    self.solution = None
+
+  def incr_ctr(self):
+    self.question_ctr += 1
+
+  def question_limit_hit(self):
+    return self.question_ctr == 20
 
   def reset(self):
-    self.conversation = [
-      {"role": "system", "content": "You are a game chat bot. Keep responses very simple."},
-      {"role": "user", "content": starter}, 
-    ]
+    self.conversation = CONVO_STUB
+    self.question_ctr = 0
 
   def ai_start_chat(self):
     response = self.client.responses.create(
     model=model,
     input=self.conversation
     )
+    solution = re.findall(r'{(.*?)}', response.output_text)
+    self.solution = solution
+    text = re.sub(r'{.*?}', '', response.output_text)
     self.conversation.append({"role": "assistant", "content": response.output_text})
-    return response.output_text
+    self.incr_ctr()
+    return text
 
   def ai_chat(self, prompt):
+    prompt = prompt + f' <System: Question number {self.question_ctr}>'
     self.conversation.append({"role": "user", "content": prompt})
     response = self.client.responses.create(
     model=model,
     input=self.conversation
     )
     self.conversation.append({"role": "assistant", "content": response.output_text})
+    self.incr_ctr()
     return response.output_text
--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from flask import Flask, request, jsonify, render_template
 import logging
 
 import ai
+from ai import ChatManager
+from random import randint
+import time
 
 app = Flask(__name__, static_url_path='/static')
 
 logger = logging.getLogger('logger')
 
-ai_object = ai.ai_object()
+chatManager = ChatManager()
 
 @app.route('/', methods=['GET'])
 def landing_page():
@@ -19,33 +22,45 @@ def landing_page():
 
 @app.route('/twenty_q', methods=['GET'])
 def twenty_q():
-
+    print("CHATS", chatManager.chat_count())
     return render_template('twenty_q.html',
                            )
 
 
 @app.route('/start_chat', methods=['POST'])
 def start_chat():
-
-    ai_object.reset()
-    reply = ai_object.ai_start_chat()
-    for x in ai_object.conversation:
+    chat_id = chatManager.add_chat()
+    reply = chatManager.start_chat(global_id=chat_id)
+    print("START CHAT", chat_id)
+    # reply = ai_object.ai_start_chat()
+    for x in chatManager.get_convo(chat_id):
         print(x)
         print("\n")
-    return jsonify({'reply': reply})
-
+    return jsonify({'reply': reply,
+                    'chat_id': chat_id
+                    })
 
 
 
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.json.get('message', '')
-    
-    reply = ai_object.ai_chat(user_message)
-    for x in ai_object.conversation:
-        print(x)
+    chat_id = int(request.json.get('chat_id'))
+
+    print(f'Chat() Chat id: {chat_id}')
+    # reply = ai_object.ai_chat(user_message)
+    reply = chatManager.send_chat(global_id=chat_id,
+                                  user_message=user_message)
+    for x in chatManager.get_convo(chat_id):
+        print(f'Chat id: {chat_id}, {x}')
         print("\n")
     return jsonify({'reply': reply})
 
 
+@app.route('/reset_chats', methods=['GET'])
+def reset_chats():
+    user_message = request.json.get('message', '')
+    chatManager.clear_all_chats()
+    ct = chatManager.chat_count()
+    return jsonify({'reply': f'Chats reset: counter {ct}'})
 
